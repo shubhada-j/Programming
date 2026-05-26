9dfd63afbcfb17133a3411f511ec1eef094c3fa6
shubhuj17 <shubhuj1027@gmail.com>
2026-05-26 23:44:27 +0530

    Add files via upload

---
+++ Practice_Programming/MultiplicationOfFactors.c
@@ -0,0 +1,33 @@
+//Accept the number from user and dispaly its multiplication of factors
+
+#include<stdio.h>
+
+int MultFact(int iNo)
+{
+    int iMult = 1;
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt<=(iNo/2); iCnt++)
+    {
+        if(iNo % iCnt == 0)
+        {
+            iMult = iMult * iCnt;   
+        }
+    }
+    return iMult;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    iRet = MultFact(iValue);
+
+    printf("%d",iRet);
+
+    return 0;
+}
