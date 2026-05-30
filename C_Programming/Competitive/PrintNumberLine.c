76c4084e8fafd3aeae0cfb2ea8c7cbd7945df225
Shubhada Jadhav <shubhadabalasahebjadhav@gmail.com>
2026-05-30 16:09:02 +0530

    Add files via upload

---
+++ C_Programming/Competitive/PrintNumberLine.c
@@ -0,0 +1,33 @@
+/*
+
+Accept the number from user and print its number line 
+Input  : 4
+Output : -4 -3 -2 -1 0 1 2 3 4 
+
+*/
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+    
+    for(iCnt = -iNo; iCnt<=iNo; iCnt++)
+    {
+        printf("%d\t",iCnt);
+    }
+}
+int main()
+{
+    int iValue = 0;
+    
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+    
+    Display(iValue);
+    
+    return 0;
+}
+
+// Time Complexity : O(N)
+// Where N >= 0
---
+++ C_Programming/Competitive/PrintOddNumbers.c
@@ -0,0 +1,35 @@
+/*
+Accept the number from user and print all odd numbers upto N
+
+Input  : 18
+Output : 1 3 5 7 9 11 13 15 17 
+*/
+
+#include<stdio.h>
+
+void OddDisplay(int iNo)
+{
+    int iCnt = 0;
+    
+    for(iCnt = 1; iCnt<=iNo; iCnt++)
+    {
+        if(iCnt % 2 != 0)
+        {
+           printf("%d\t",iCnt);
+        }
+    }
+}
+int main()
+{
+    int iValue = 0;
+    
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+    
+    OddDisplay(iValue);
+    
+    return 0;
+}
+
+// Time Complexity : O(N)
+// Where N >= 0
