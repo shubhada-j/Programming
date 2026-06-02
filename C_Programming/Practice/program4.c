1bc99442e61efa84f58ae76f9a65199d46cb9009
Shubhada Jadhav <shubhadabalasahebjadhav@gmail.com>
2026-06-02 23:42:10 +0530

    Add files via upload

---
+++ C_Programming/Practice/program4.c
@@ -0,0 +1,20 @@
+//Take input from user and print the addition of two numbers
+
+#include<stdio.h>
+
+int main()
+{
+    int i,j,k;
+
+    printf("Enter first number : \n");
+    scanf("%d",&i);
+
+    printf("Enter second number : \n");
+    scanf("%d",&j);
+
+    k = i + j;
+
+    printf("Addition is : %d\n",k);
+
+    return 0;
+}
---
+++ C_Programming/Practice/program5.c
@@ -0,0 +1,31 @@
+/*
+    Algorithm
+
+    START
+        Accept first number as No1
+        Accept second number as No2
+        Perform Addition of No1 and No2
+        Display the Result
+    STOP
+*/
+
+#include<stdio.h>
+
+int main()
+{                           
+    /*changing the datatype int -> float 
+    because in int point valus cannot accepeted*/
+    float i,j,k;
+
+    printf("Enter first number : \n");
+    scanf("%f",&i);
+
+    printf("Enter second number : \n");
+    scanf("%f",&j);
+
+    k = i + j;
+    
+    printf("Addition is : %f\n",k);
+
+    return 0;
+}
