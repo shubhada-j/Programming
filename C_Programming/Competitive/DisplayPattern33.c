5d05a3796fb6423bd5846813c90a8600ddb01aa7
Shubhada Jadhav <shubhadabalasahebjadhav@gmail.com>
2026-06-18 11:18:14 +0530

    Add files via upload

---
+++ C_Programming/Competitive/DisplayPattern33.c
@@ -0,0 +1,42 @@
+/*
+Input  :    iRow = 6     iCol = 6
+Output :    *   *   *   *   *   *
+            *               *   *
+            *           *       *
+            *       *           *
+            *   *               *
+            *   *   *   *   *   *
+*/
+
+#include<stdio.h>
+void Pattern(int iRow,int iCol)
+{
+    int i = 0, j = 0;
+   
+    for(i = iRow; i >= 1; i--)
+    {
+        for(j = 1; j <= iCol; j++)
+        { 
+            if((i == j)||(i == iRow)||(j == iCol)||(i == 1)||(j == 1))
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf(" \t");
+            }
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and columns : \n");
+    scanf("%d %d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
