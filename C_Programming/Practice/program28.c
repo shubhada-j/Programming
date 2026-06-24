3ab316d76b8f6beba030262819dd60af3b9584b4
Shubhada Jadhav <shubhadabalasahebjadhav@gmail.com>
2026-06-24 23:55:52 +0530

    Add files via upload

---
+++ C_Programming/Practice/program28.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("Jay Ganesh...\n");
+    }
+}
+int main()
+{
+    int iValue = 0;
+
+    printf("Enetr the Frequency : ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
