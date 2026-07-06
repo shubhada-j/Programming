7ac355f476482ede3d3215c2bdc81b18386bad92
Shubhada Jadhav <shubhadabalasahebjadhav@gmail.com>
2026-07-06 23:55:46 +0530

    Add files via upload

---
+++ Java_Programming/Practice/program37.java
@@ -0,0 +1,17 @@
+/////////////////////////////////////////////////////////
+//Type 1 (Print statement)
+
+
+class program37
+{
+    public static void main(String A[])
+    {
+        int iCnt = 0;
+
+        for(iCnt = 1; iCnt<=10; iCnt++)
+        {
+            System.out.println(iCnt);
+        }
+    }
+
+}
