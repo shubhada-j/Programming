a3675e6f0859df0b6f723286914af0eda1cc5aa1
Shubhada Jadhav <shubhadabalasahebjadhav@gmail.com>
2026-07-23 02:02:19 +0530

    Update and rename first.cpp to GenericCountFreq.cpp

---
+++ C++_Programming/Competative/GenericCountFreq.cpp
@@ -0,0 +1,33 @@
+//Write generic program to accept N values and count frequency of any specific value
+
+#include<iostream>
+using namespace std;
+
+template<class T>
+int Frequency(T *arr, int iSize, T iNo)
+{
+    int i = 0;
+    int iCount = 0; 
+
+    for (i = 0; i<iSize; i++)
+    {
+        if(arr[i] == iNo)
+        {
+            iCount++;
+        }
+    }
+
+    return iCount;
+}
+
+
+int main()
+{
+    int arr[] = {10,20,30,10,30,40,10,40,10};
+
+    int iRet = Frequency(arr,9,10);
+
+    cout<<iRet;
+
+    return 0;
+}
