f979da2b835a388df5c381e6b614ac9a69db7cad
Shubhada Jadhav <shubhadabalasahebjadhav@gmail.com>
2026-08-09 23:34:54 +0530

    Add files via upload

---
+++ C_Programming/Practice/program52.c
@@ -0,0 +1,26 @@
+//Input  : 5
+//Output : 1 2 3 4 5
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+    
+    for(iCnt = 1; iCnt<=iNo; iCnt++)
+    {
+        printf("%d\t",iCnt);                // \t -> tab
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the number : ");
+    scanf("%d\n",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
