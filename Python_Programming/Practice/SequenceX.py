630d7c209b685874dd866ef5e941f68c6cff7d3f
Shubhada Jadhav <shubhadabalasahebjadhav@gmail.com>
2026-08-15 23:54:44 +0530

    Add files via upload

---
+++ Python_Programming/Practice/SequenceX.py
@@ -0,0 +1,11 @@
+Data1 = [10,20,30,40]                          
+Data2 = (10,20,30,40)                           
+Data3 = {10,20,30,40}                           
+Data4 = {"A":10, "B":20, "C":30, "D":40}       
+Data5 = "Maharashtra"                           
+
+print(type(Data1))
+print(type(Data2))
+print(type(Data3))
+print(type(Data4))
+print(type(Data5))
---
+++ Python_Programming/Practice/SequenceXX.py
@@ -0,0 +1,9 @@
+print("Enter the first number : ")
+No1 = int(input())
+
+print("Enter the second number : ")
+No2 = int(input())
+
+Ans = No1 * No2
+
+print("Multiplication is :",Ans)
