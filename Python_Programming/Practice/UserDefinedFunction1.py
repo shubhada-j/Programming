573f628423c9a364d42f1fd2869d155b3d69ef86
Shubhada Jadhav <shubhadabalasahebjadhav@gmail.com>
2026-08-22 23:59:48 +0530

    Add files via upload

---
+++ Python_Programming/Practice/UserDefinedFunction1.py
@@ -0,0 +1,10 @@
+# Accept :  Nothing
+# Return :  Nothing``
+
+def Marvellous():
+    print("Inside Marvellous")
+
+def main():
+    Marvellous()
+if __name__ == "__main__":
+    main()
---
+++ Python_Programming/Practice/UserDefinedFunction2.py
@@ -0,0 +1,10 @@
+# Accept :  One parameter
+# Return :  Nothing
+
+def Marvellous(Value):
+    print("Inside Marvellous : ",Value)
+
+def main():
+    Marvellous(11)
+if __name__ == "__main__":
+    main()
