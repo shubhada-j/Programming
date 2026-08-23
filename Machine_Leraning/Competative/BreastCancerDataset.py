766b354d35ea64e43f96e391a371f01aa3ee6579
Shubhada Jadhav <shubhadabalasahebjadhav@gmail.com>
2026-08-23 23:50:16 +0530

    Add files via upload

---
+++ Machine_Leraning/Competative/BreastCancerDataset.py
@@ -0,0 +1,16 @@
+
+import pandas as pd
+from sklearn.metrics import accuracy_score,confusion_matrix
+from sklearn.
+
+
+def main():
+    df = pd.read_csv("breast_cancer.csv")
+
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
