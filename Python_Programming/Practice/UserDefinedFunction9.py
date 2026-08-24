04fdcd2d0d01f994bbec96eedaffa900b3b9a7a4
Shubhada Jadhav <shubhadabalasahebjadhav@gmail.com>
2026-08-24 23:50:47 +0530

    Add files via upload

---
+++ Python_Programming/Practice/UserDefinedFunction9.py
@@ -0,0 +1,17 @@
+def BigBazar():
+    print("Inside BigBazar")
+
+    def Amul():
+        print("Inside Amul icecream parlour")
+
+    Amul()
+    Amul()
+
+
+def main():
+    BigBazar()            
+
+
+if __name__ == "__main__":
+    main()
+
