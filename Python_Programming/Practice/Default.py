c3ca2594d9ee1428e8f37f6ad58a7937325f53cf
Shubhada Jadhav <shubhadabalasahebjadhav@gmail.com>
2026-08-26 23:52:45 +0530

    Add files via upload

---
+++ Python_Programming/Practice/Default.py
@@ -0,0 +1,16 @@
+def Area(Radius, PI=3.14):
+    Ans =  PI * Radius * Radius
+    return Ans
+
+def main():
+    Ret = Area(10.5)
+    print("Area of Circle is : ",Ret)
+
+    Ret = Area(10.5,7.12)
+    print("Area of Circle is : ",Ret)
+
+    
+
+
+if __name__ == "__main__":
+    main()
