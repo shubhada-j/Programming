3f0e58dddba0786bfd9f503cc4375c97f7ac18aa
Shubhada Jadhav <shubhadabalasahebjadhav@gmail.com>
2026-09-03 23:13:13 +0530

    Add files via upload

---
+++ Python_Programming/Practice/DynamicInputList.py
@@ -0,0 +1,17 @@
+def main():
+   size = 0
+   Arr = list()
+
+   print("Enter the number of elements : ")
+   Size = int(input())
+
+   print("Enter the elements : ")
+
+   for i in range(Size):
+      no = int(input())
+      Arr.append(no)
+
+   print(Arr)
+
+if __name__ == "__main__":
+    main()
