0f63be577e193870a64d9525838246c2bb299596
Shubhada Jadhav <shubhadabalasahebjadhav@gmail.com>
2026-09-13 00:06:16 +0530

    Add files via upload

---
+++ Python_Programming/Practice/ExceptionDemo6.py
@@ -0,0 +1,31 @@
+def main():
+   Ans = 0
+
+   try:
+
+      print("Enter first number : ")
+      No1 = int(input())
+
+      print("Enter second number : ")
+      No2 = int(input())
+
+      Ans = No1 / No2
+
+      print("Division is successfull")
+
+   except ZeroDivisionError as zobj:                                                                
+      print("Exception occur due to 2nd operand (No2) is 0 : ",zobj)
+
+   except ValueError as vobj:                                              
+      print("Exception occured due to invalid datatype : ",vobj)
+
+   except Exception as eobj:                                               
+      print("Exception Occured ",eobj)
+
+   finally:
+      print("Inside finally block")
+   
+   print("Result is : ",Ans)
+
+if __name__ =="__main__":
+    main()
