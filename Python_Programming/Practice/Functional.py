276a0eeda8d95b4e2af1b8c096c71ee4732ec7fa
Shubhada Jadhav <shubhadabalasahebjadhav@gmail.com>
2026-09-14 23:54:52 +0530

    Add files via upload

---
+++ Python_Programming/Practice/Functional.py
@@ -0,0 +1,15 @@
+Addition = lambda No1,No2 : No1 + No2
+
+Substrction = lambda No1,No2 : No1 - No2
+
+print("Enter first number")
+Value1 = int(input())
+
+print("Enter second number")
+Value2 = int(input())
+
+Ret = Addition(Value1,Value2)
+print("Addition is : ",Ret)
+
+Ret = Substrction(Value1,Value2)
+print("Substrction is : ",Ret)
---
+++ Python_Programming/Practice/Instancevariable2.py
@@ -0,0 +1,17 @@
+class Marvellous:
+    No1 = 11
+    No2 = 12
+
+    def __init__(self):
+        self.Value1 = 21
+        self.Value2 = 51
+
+print(Marvellous.No1)
+print(Marvellous.No2)
+
+mobj1 = Marvellous()
+mobj2 = Marvellous()
+mobj3 = Marvellous()
+
+print(mobj1.Value1)
+print(mobj2.Value1)
